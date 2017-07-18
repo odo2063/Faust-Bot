348e45592f6f23779988766fa7b25e8461dd72d5
odo2063 <odo2063@users.noreply.github.com>
2017-07-18 12:26:16 +0200

    ADD Afterwork drinks

--- FaustBot/Modules/GiveDrinkObserver.py
+++ FaustBot/Modules/GiveDrinkObserver.py
@@ -1,7 +1,7 @@
 from FaustBot.Communication.Connection import Connection
 from FaustBot.Modules.PrivMsgObserverPrototype import PrivMsgObserverPrototype
-from getraenke import getraenke
-import random
+from getraenke import getraenke, getraenke_after_work
+import random, datetime
 
 
 class GiveDrinkObserver(PrivMsgObserverPrototype):
@@ -9,4 +9,12 @@ class GiveDrinkObserver(PrivMsgObserverPrototype):
     def update_on_priv_msg(self, data: dict, connection: Connection):
         if data['message'].find('.drink') == -1:
             return
-        connection.send_back('\001ACTION schenkt ' + data['nick'] + ' ' + random.choice(getraenke) + ' ein.\001', data)
+
+        now = datetime.datetime.now()
+
+        if (( now.hour ) >= 22) or (( now.hour ) < 5): #alternativ nur am Wochenende oder sowas
+            print('afterwork')
+            connection.send_back('\001ACTION schenkt ' + data['nick'] + ' ' + random.choice(getraenke + getraenke_after_work) + ' ein.\001', data)
+        else:
+            print('work')
+            connection.send_back('\001ACTION schenkt ' + data['nick'] + ' ' + random.choice(getraenke) + ' ein.\001', data)
